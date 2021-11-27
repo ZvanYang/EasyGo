aa4ac808c317c6af74b87b3cf152cb2c44f90afc
ZvanYang <zywangzhaoyang@163.com>
2021-11-27 17:45:46 +0800

    实现端口监听

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	http.HandleFunc("/", hello)
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		fmt.Println("error message:", err)
+	}
+}
+
+func hello(wr http.ResponseWriter, r *http.Request)  {
+	write, err := wr.Write([]byte("Hello World"))
+	if err != nil {
+		return 
+	}
 }
